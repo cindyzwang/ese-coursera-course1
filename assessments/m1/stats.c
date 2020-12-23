e9ea17392fef49ef47ace78ea3d85667291f1608
Cindy Wang <czwang113@gmail.com>
2020-12-23 00:12:05 -0700

    Implement methods and compile

--- assessments/m1/stats.c
+++ assessments/m1/stats.c
@@ -27,7 +27,7 @@
 /* Size of the Data Set */
 #define SIZE (40)
 
-void main() {
+int main() {
 
   unsigned char test[SIZE] = { 34, 201, 190, 154,   8, 194,   2,   6,
                               114, 88,   45,  76, 123,  87,  25,  23,
@@ -37,7 +37,74 @@ void main() {
 
   /* Other Variable Declarations Go Here */
   /* Statistics and Printing Functions Go Here */
-
+  print_statistics(test);
+  return 0;
 }
 
 /* Add other Implementation File Code Here */
+void print_statistics(unsigned char data[]) {
+  double median = find_median(data, SIZE);
+  double mean = find_mean(data, SIZE);
+  int maximum = find_maximum(data, SIZE);
+  int minimum = find_minimum(data, SIZE);
+  printf("Median: %.2f\n", median);
+  printf("Mean: %.2f\n", mean);
+  printf("Minimum: %i\n", minimum);
+  printf("Maximum: %i\n", maximum);
+}
+
+void print_array(unsigned char data[], int length) {
+  for (int i = 0; i < length - 1; i++) {
+    printf("%i, ", data[i]);
+  }
+  printf("%i\n", data[length - 1]);
+}
+
+int find_median(unsigned char data[], int length) {
+  sort_array(data, length); // ensure the data is sorted
+  unsigned char is_odd = length % 2;
+  if (is_odd) {
+    int median_1 = (double) data[length / 2];
+    int median_2 = (double) data[(length / 2) + 1];
+    return (median_1 + median_2) / 2.0;
+  } else {
+    return data[length / 2];
+  }
+}
+
+double find_mean(unsigned char data[], int length) {
+  sort_array(data, length);  // ensure the data is sorted
+  int sum = 0;
+  for (int i = 0; i < length; i++) {
+    sum += data[i];
+  }
+
+  return (double) sum / length;
+}
+
+int find_maximum(unsigned char data[], int length) {
+  sort_array(data, length);  // ensure the data is sorted
+  return data[0];
+}
+
+int find_minimum(unsigned char data[], int length) {
+  sort_array(data, length);  // ensure the data is sorted
+  return data[length - 1];
+}
+
+void sort_array(unsigned char data[], int length) {
+  // implementation of bubble sort
+  for (int i = 0; i < length; i++) {
+    unsigned char swapped = 0;
+    for (int j = 0; j < length - i - 1; j++) {
+      if (data[j] < data[j + 1]) {
+        unsigned char temp = data[j];
+        data[j] = data[j + 1];
+        data[j + 1] = temp;
+        if (!swapped) swapped = 1;
+      }
+    }
+    if (!swapped) break;  // everything is sorted, no more iterations necessary
+  }
+}
+
